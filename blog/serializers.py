b3725853497338b96ded2d05e3216246bcb48e8e
zeonga1102 <dlwjddk4587@naver.com>
2022-06-26 00:16:17 +0900

    ENH: blog에서 Article get, post 할 때 serializers 이용하도록 변경

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Article, Comment
+from .models import Article, Comment, Category
 
 class CommentSerializer(serializers.ModelSerializer):
    class Meta:
@@ -7,8 +7,56 @@ class CommentSerializer(serializers.ModelSerializer):
       # all 보다는 구체적인 필드를 명시하는 것이 좋음
       fields = "__all__"
 
+
+class CategorySerializer(serializers.ModelSerializer):
+   class Meta:
+      model = Category
+      fields = "__all__"
+
+
 class ArticleSerializer(serializers.ModelSerializer):
-   comment_set = CommentSerializer(many=True)
+   comment_set = CommentSerializer(many=True, read_only=True)
+   author_name = serializers.SerializerMethodField(read_only=True)
+   category_name = serializers.SerializerMethodField(read_only=True)
+
+   def get_category_name(self, obj):
+      return [ category.name for category in obj.category.all() ]
+
+   def get_author_name(self, obj):
+      return obj.author.username
+
+
+   def validate(self, data):
+      if len(data.get('title')) <= 5:
+        raise serializers.ValidationError(
+                   # custom validation error message
+                   detail={"error": "제목은 5글자를 넘아야 합니다."},
+               )
+
+      if len(data.get('content')) <= 20:
+         raise serializers.ValidationError(
+                  # custom validation error message
+                  detail={"error": "내용은 20글자를 넘어야 합니다."},
+               )
+      if not data.get('category', None):
+         raise serializers.ValidationError(
+                  # custom validation error message
+                  detail={"error": "카테고리를 지정해야 합니다."},
+               )
+
+      return data
+
+   
+   def create(self, validated_data):
+      categories = validated_data.pop('category')
+      
+      article = Article(**validated_data)
+      article.save()
+      article.category.set(categories)
+      
+      return validated_data
+
+
    class Meta:
       model = Article
-      fields = "__all__"
+      fields = ['id', 'category', 'category_name', 'title', 'author', 'author_name', 'content', 'comment_set']
--- blog/views.py
+++ blog/views.py
@@ -1,39 +1,27 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import permissions
+from rest_framework import status
 from django.utils import timezone
-from .models import Article, Category
+from .models import Article
 from drfClass.permissions import IsAdminOrRegisteredMoreThanAWeekUserOrIsAuthenticatedReadOnly
+from .serializers import ArticleSerializer
 
-# Create your views here.
 class BlogView(APIView):
     permission_classes = [IsAdminOrRegisteredMoreThanAWeekUserOrIsAuthenticatedReadOnly]
 
     def get(self, request):
         time = timezone.now()
         articles = Article.objects.filter(author=request.user, start_date__lt=time, end_date__gt=time).order_by('-create_date')
-        titles = [ article.title for article in articles ]
-        return Response({'titles': titles})
+        article_serializer = ArticleSerializer(articles, many=True).data
+
+        return Response(article_serializer, status=status.HTTP_200_OK)
     
     def post(self, request):
-        author = request.user
-        title = request.data.get('title', '')
-        category_name = request.data.get('category_name', '')
-        content = request.data.get('content', '')
+        request.data['author'] = request.user.id
+        article_serializer = ArticleSerializer(data=request.data)
 
-        # if문들 validator에서 처리해주면 좋음
-        if len(title) <= 5:
-            return Response({'message': '제목은 5글자를 넘아야 합니다.'})
+        if article_serializer.is_valid():
+            article_serializer.save()
+            return Response({"message": "정상"}, status=status.HTTP_200_OK)
         
-        if len(content) <= 20:
-            return Response({'message': '내용은 20글자를 넘어야 합니다.'})
-        
-        if category_name:
-            category = [ Category.objects.get(name=name) for name in category_name.split(',') ]
-        else:
-            return Response({'message': '카테고리를 지정해야 합니다.'})
-
-        new_article = Article.objects.create(author=author, title=title, content=content)
-        new_article.category.set(category)
-
-        return Response({'message': '업로드 성공!'})
+        return Response(article_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
