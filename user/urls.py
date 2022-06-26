e2be66dfb9554498083494761a44d7c879c1cb06
zeonga1102 <dlwjddk4587@naver.com>
2022-06-26 19:11:16 +0900

    ENH: /logout url name 설정

--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('signup/', views.UserView.as_view(), name='signup'),
     path('login/', views.UserApiView.as_view(), name='login'),
-    path('logout/', views.UserApiView.as_view()),
+    path('logout/', views.UserApiView.as_view(), name='logout'),
     path('info/', views.UserInfoView.as_view(), name='user_info'),
     path('', views.IndexView.as_view(), name='index')
 ]
