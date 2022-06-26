59560faf078f9ab9d2d8f1c9749089f1fd7ba708
zeonga1102 <dlwjddk4587@naver.com>
2022-06-26 18:04:23 +0900

    ENH: signup.html, login.html, user_info.html. render

--- user/urls.py
+++ user/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.UserView.as_view()),
-    path('login/', views.UserApiView.as_view()),
+    path('signup/', views.UserView.as_view(), name='signup'),
+    path('login/', views.UserApiView.as_view(), name='login'),
     path('logout/', views.UserApiView.as_view()),
-    path('info/', views.UserInfoView.as_view()),
+    path('info/', views.UserInfoView.as_view(), name='user_info'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from rest_framework.renderers import TemplateHTMLRenderer
 from django.contrib.auth import login, logout, authenticate
 from rest_framework.views import APIView
@@ -9,11 +9,10 @@ from user.serializers import UserSerializer
 class UserView(APIView):
     permission_classes = [permissions.AllowAny]
 
-    # renderer_classes = [TemplateHTMLRenderer]
-    # template_name = 'signup.html'
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'signup.html'
 
     def get(self, request):
-        # return render(request, 'signup.html')
         return Response({"message": "정상"}, status=status.HTTP_200_OK)
     
     # 회원가입
@@ -33,6 +32,12 @@ class UserView(APIView):
         return Response({'message': 'delete method'})
 
 class UserApiView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'login.html'
+
+    def get(self, request):
+        return Response({"message": "정상"}, status=status.HTTP_200_OK)
+
     # 로그인
     def post(self, request):
         username = request.data.get('username', '')
@@ -43,16 +48,20 @@ class UserApiView(APIView):
             return Response({"error": "존재하지 않는 계정이거나 패스워드가 일치하지 않습니다."}, status=status.HTTP_401_UNAUTHORIZED)
 
         login(request, user)
-        return Response({"message": "로그인 성공!!"}, status=status.HTTP_200_OK)
+        return redirect('user_info')
+        # return Response({"message": "로그인 성공!!"}, status=status.HTTP_200_OK)
 
     # 로그아웃
     def delete(self, request):
         logout(request)
-        return Response({'message': '로그아웃 성공'})
+        return redirect('login')
 
 class UserInfoView(APIView):
     permission_classes = [permissions.IsAuthenticated]
     
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'user_info.html'
+
     # 유저 기본 정보 및 프로필 조회
     def get(self, request):
         user = request.user
