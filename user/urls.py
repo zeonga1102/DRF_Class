450033fc65ef8415f15e7e0dff576723fde8dac5
zeonga1102 <dlwjddk4587@naver.com>
2022-06-27 15:04:47 +0900

    ENH: 이미지 파일 url 추가

--- user/urls.py
+++ user/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -7,4 +9,4 @@ urlpatterns = [
     path('logout/', views.UserApiView.as_view(), name='logout'),
     path('info/', views.UserInfoView.as_view(), name='user_info'),
     path('', views.IndexView.as_view(), name='index')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
